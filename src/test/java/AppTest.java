401fdec6f093f3da552454d0483501f86934e446
Banciu Raul <raul_banciu@yahoo.com>
2019-03-15 20:16:07 +0200

    comit 2

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,10 +1,9 @@
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import domain.Student;
 import org.junit.Test;
 import repository.StudentFileRepository;
 import validation.StudentValidator;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
@@ -27,6 +26,16 @@ public class AppTest
         Student s1 = new Student("1313", "Bizonu",932);
         repo.save(s1);
         assertEquals(repo.findOne("1313").getGrupa(),932);
+
+    }
+
+    @Test
+    public void testAddSTudentDuplicate(){
+        StudentValidator validator = new StudentValidator();
+        StudentFileRepository repo = new StudentFileRepository(validator,"E:\\Lecture02MaxPointsParticipants\\files\\studenti.txt");
+        Student s1 = new Student("1313", "Bizonul",932);
+        repo.save(s1);
+        assertNotEquals(repo.findOne("1313").getGrupa(),"Bizonul");
 
     }
 }
